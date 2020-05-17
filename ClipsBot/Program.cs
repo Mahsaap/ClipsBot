1d063648f4cb6d3b61baf5fbd888ec9cb1b5ded0
Mahsaap <vimhatt@hotmail.com>
2020-05-16 22:10:12 -0300

    Initial Commit

---
+++ ClipsBot/Program.cs
@@ -0,0 +1,24 @@
+﻿using ClipsBot.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace ClipsBot
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).RunConsoleAsync();
+        }
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddHostedService<LifetimeMonitoringHostedService>();
+            })
+            .UseConsoleLifetime();
+    }
+}
+    
+
