5d21cb2ca810b0d9420880680842b18ce40674af
tim.hatt <tim.hatt@innovasea.com>
2020-06-02 13:11:08 -0300

    add version / user ids

--- ClipsBot/Globals.cs
+++ ClipsBot/Globals.cs
@@ -2,6 +2,18 @@
 
 namespace ClipsBot
 {
+    public class Version
+    {
+        public static string Major { get { return "1"; } }
+        public static string Minor { get { return "0"; } }
+    }
+
+    public class DiscordIDs
+    {
+        public static ulong Mahsaap { get { return 88798728948809728; } }
+        public static ulong Nizcik { get { return 290501197255671809; } }
+    }
+
     static class Globals
     {
         public static string CurrentTime { get { return DateTime.Now.ToString("HH:mm:ss"); } }
