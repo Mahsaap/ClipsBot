5b81c8f15451b5b2e15162821e65fbdcb4056caa
tim.hatt <tim.hatt@innovasea.com>
2020-06-02 13:11:24 -0300

    RequireID precondition

---
+++ ClipsBot/Preconditions/RequireID.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClipsBot.Preconditions
+{
+    class RequireID : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            var ids = new List<ulong> { DiscordIDs.Mahsaap, DiscordIDs.Nizcik };
+            if (ids.Contains(context.User.Id))
+            {
+                return Task.FromResult(PreconditionResult.FromSuccess());
+            }
+            else
+                return Task.FromResult(PreconditionResult.FromError("You do not have the required user ID"));
+        }
+    }
+}
